6152357a76ab1774223c6fb0ed17904b2a369895
zhpjy <zhpjy@qq.com>
2020-04-23 22:29:53 +0800

    [fet]:为防御塔充能

--- role.harvester.js
+++ role.harvester.js
@@ -7,7 +7,7 @@ var roleHarvester = {
         function work(creep) {
             var targets = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                    return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_TOWER) &&
                         structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
                 }
             });
