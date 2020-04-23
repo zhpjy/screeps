080864b2e221fbe735967515aa989d32d0c35556
JesseTzh <jessetzh@gmail.com>
2020-04-23 17:32:27 +0800

    尝试优化Creep生成方法

--- component.creepsManager.js
+++ component.creepsManager.js
@@ -9,36 +9,39 @@ function autoClean(){
     }
 }
 
+//检测Spawns中是否有足够Energy
+function checkSpawnsEnergy(){
+    if(Game.spawns[CONFIG.SPAWN_NAME].energy >= 200){
+        return true;
+    }
+}
+
+//生成Creep，日后如memory中有更多参数需要传入再行更改
+function generateCreep(creepRole){
+    if(checkSpawnsEnergy()){
+        var newName = creepRole + Game.time;
+        console.log('Spawning a new creep: ' + newName);
+        Game.spawns[CONFIG.SPAWN_NAME].spawnCreep(CONFIG.WORKER_TEMPLATE, newName, {memory: {role: creepRole}});
+    }
+}
+
 function autoGenerate(){
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
     var upgrader = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
     console.log("harvesters:"+harvesters.length+" builders"+builders.length+" upgrader:" + upgrader.length)
 
-    if(Game.spawns[CONFIG.SPAWN_NAME].energy<200){
-        return;
+    //使用while以实现如资源不足则在当前循环内不断等待，而不是打断整个函数执行
+    while(harvesters.length<CONFIG.HARVESTER_NUM){
+        generateCreep('harvester');
     }
 
-    if(harvesters.length<CONFIG.HARVESTER_NUM){
-        var newName = 'Harvester' + Game.time;
-        console.log('Spawning new harvester: ' + newName);
-        Game.spawns[CONFIG.SPAWN_NAME].spawnCreep(CONFIG.WORKER_TEMPLATE, newName, {memory: {role: 'harvester'}});
-        //采矿工人数目不达标不生产别的工人
-        return;
+    while(upgrader.length < CONFIG.UPGRASER_NUM) {
+        generateCreep('upgrader');
     }
 
-    if(upgrader.length < CONFIG.UPGRASER_NUM) {
-        var newName = 'Upgrader' + Game.time;
-        console.log('Spawning new upgrader: ' + newName);
-        Game.spawns[CONFIG.SPAWN_NAME].spawnCreep(CONFIG.WORKER_TEMPLATE, newName, 
-            {memory: {role: 'upgrader'}});        
-    }
-
-    if(builders.length < CONFIG.BUILDER_NUM) {
-        var newName = 'Builder' + Game.time;
-        console.log('Spawning new builder: ' + newName);
-        Game.spawns[CONFIG.SPAWN_NAME].spawnCreep(CONFIG.WORKER_TEMPLATE, newName, 
-            {memory: {role: 'builder'}});        
+    while(builders.length < CONFIG.BUILDER_NUM) {
+        generateCreep('builder');
     }
 }
 
