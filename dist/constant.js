924b5151156902af33a678b5b36103a3571072a0
zhpjy <zhpjy@qq.com>
2020-05-15 15:12:05 +0800

    提交dist

---
+++ dist/constant.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ActionReturnEnum;
+(function (ActionReturnEnum) {
+    ActionReturnEnum[ActionReturnEnum["NEED_STOP"] = -10000] = "NEED_STOP";
+})(ActionReturnEnum = exports.ActionReturnEnum || (exports.ActionReturnEnum = {}));
