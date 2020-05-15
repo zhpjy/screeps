90e2119afc07420b72d63132d2f9e99b3c556190
zhpjy <zhpjy@qq.com>
2020-05-15 15:13:22 +0800

    删除dist

--- dist/constant.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ActionReturnEnum;
-(function (ActionReturnEnum) {
-    ActionReturnEnum[ActionReturnEnum["NEED_STOP"] = -10000] = "NEED_STOP";
-})(ActionReturnEnum = exports.ActionReturnEnum || (exports.ActionReturnEnum = {}));
